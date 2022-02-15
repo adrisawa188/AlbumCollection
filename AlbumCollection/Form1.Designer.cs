b5fd820d594c29ebf8438929ae94cb68179f618b
adrisawa188 <adrisawa188@SDS-L-BB1HC63>
2022-02-15 09:56:50 -0500

    Add project files.

---
+++ AlbumCollection/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace AlbumCollection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.orginalOrderLabel = new System.Windows.Forms.Label();
+            this.sortedOrderLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(223, 46);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(386, 26);
+            this.inputBox.TabIndex = 0;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(615, 46);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 26);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(696, 46);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 26);
+            this.removeButton.TabIndex = 2;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Insert Information here:";
+            // 
+            // orginalOrderLabel
+            // 
+            this.orginalOrderLabel.BackColor = System.Drawing.Color.Goldenrod;
+            this.orginalOrderLabel.Location = new System.Drawing.Point(12, 128);
+            this.orginalOrderLabel.Name = "orginalOrderLabel";
+            this.orginalOrderLabel.Size = new System.Drawing.Size(372, 313);
+            this.orginalOrderLabel.TabIndex = 4;
+            // 
+            // sortedOrderLabel
+            // 
+            this.sortedOrderLabel.BackColor = System.Drawing.Color.Goldenrod;
+            this.sortedOrderLabel.Location = new System.Drawing.Point(416, 128);
+            this.sortedOrderLabel.Name = "sortedOrderLabel";
+            this.sortedOrderLabel.Size = new System.Drawing.Size(372, 313);
+            this.sortedOrderLabel.TabIndex = 5;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.DarkRed;
+            this.outputLabel.Location = new System.Drawing.Point(12, 88);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(776, 23);
+            this.outputLabel.TabIndex = 7;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.sortedOrderLabel);
+            this.Controls.Add(this.orginalOrderLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.inputBox);
+            this.Name = "Form1";
+            this.Text = "AlbumCollection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label orginalOrderLabel;
+        private System.Windows.Forms.Label sortedOrderLabel;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ AlbumCollection/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlbumCollection
+{
+    public partial class Form1 : Form
+    {
+        List<string> oOrder = new List<string>();
+        List<string> sOrder = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            orginalOrderLabel.Text = "Original List ------------\n\n";
+            sortedOrderLabel.Text = "Sorted List ------------\n\n";
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string userInput = inputBox.Text;
+
+            oOrder.Add(userInput);
+            sOrder.Add(userInput);
+            sOrder.Sort();
+
+            //display info as it was orginally inputted
+            orginalOrderLabel.Text = "Original List ------------\n\n";
+            for (int i = 0; i < oOrder.Count(); i++)
+            {
+                orginalOrderLabel.Text += "\n" + oOrder[i];
+            }
+
+            //display info sorted alphabetically 
+            sortedOrderLabel.Text = "Sorted List ------------\n\n";
+            for (int i = 0; i < sOrder.Count(); i++)
+            {
+                sortedOrderLabel.Text += "\n" + sOrder[i];
+            }
+
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            string userInput = inputBox.Text;
+            outputLabel.Text = "";
+
+
+            if (oOrder.Contains(userInput))
+            {
+                oOrder.Remove(userInput);
+                sOrder.Remove(userInput);
+                
+                sOrder.Sort();
+
+                //display info as it was orginally inputted
+                orginalOrderLabel.Text = "Original List ------------\n\n";
+                for (int i = 0; i < oOrder.Count(); i++)
+                {
+                    orginalOrderLabel.Text += "\n" + oOrder[i];
+                }
+
+                //display info sorted alphabetically 
+                sortedOrderLabel.Text = "Sorted List ------------\n\n";
+                for (int i = 0; i < sOrder.Count(); i++)
+                {
+                    sortedOrderLabel.Text += "\n" + sOrder[i];
+                }
+
+            }
+            else
+            {
+                outputLabel.Text = "Please enter a valid name";
+            }
+        }
+    }
+}
